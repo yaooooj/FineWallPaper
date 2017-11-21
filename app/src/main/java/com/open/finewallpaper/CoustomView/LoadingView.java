8955698019662b2c642684e2e9ce2b76203a1099
yaojian <yaooooj@gmail.com>
2017-11-22 01:10:55 +0800

    优化异常布局

--- app/src/main/java/com/open/finewallpaper/CoustomView/LoadingView.java
+++ app/src/main/java/com/open/finewallpaper/CoustomView/LoadingView.java
@@ -5,15 +5,18 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.open.finewallpaper.R;
+import com.open.finewallpaper.Util.DisplayUtil;
 
 /**
  * Created by yaojian on 2017/11/20.
@@ -32,6 +35,10 @@ public class LoadingView  extends View{
 
     private int height ;
     private int width ;
+
+    private Path path;
+
+    private Path textPath;
     public LoadingView(Context context) {
         super(context);
         init(context);
@@ -63,13 +70,29 @@ public class LoadingView  extends View{
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setColor(Color.BLACK);
 
+
+
+
+        int screenWidth = DisplayUtil.getsWidthPixles(context);
+        int screenHeight = DisplayUtil.getsHightPixles(context);
+        float ratioWidth = (float)screenWidth / 720;
+        float ratioHeight = (float)screenHeight / 1080;
+        float RATIO = Math.min(ratioWidth, ratioHeight);
+        int textSize = Math.round(20 * RATIO);
+
+
         textPaint = new Paint();
-        textPaint.setColor(textColor);
+        textPaint.setColor(Color.BLACK);
         textPaint.setAntiAlias(true);
+        textPaint.setStyle(Paint.Style.STROKE);
+        textPaint.setStrokeWidth(2f);
         textPaint.setTextAlign(Paint.Align.CENTER);
-        textPaint.setTextSize(16);
+        textPaint.setTextSize(textSize);
         //RectF rect = new RectF(3 * width / 8,(height / 3) - (width / 8) ,8 * width / 5,height / 3);
         rect = new RectF(0,0,200,200);
+
+
+
     }
 
     public void setText(String text){
@@ -87,37 +110,38 @@ public class LoadingView  extends View{
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
-        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
-        int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
-        int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
 
-        if (widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST){
-            setMeasuredDimension(100,100);
-        }else if (widthMeasureSpec == MeasureSpec.AT_MOST ){
-            setMeasuredDimension(100,heightSpecSize);
-        }else if (heightSpecMode == MeasureSpec.AT_MOST){
-            setMeasuredDimension(widthSpecSize,100);
-        }
     }
 
     @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = w;
+        height = h;
+        path = new Path();
+        path.moveTo(3 * width / 8,height / 2 - width / 8 );
+        path.quadTo(width / 2, height / 6, 5 * width / 8,height / 2 - width / 8 );
+
+        textPath = new Path();
+        textPath.moveTo(3 * width / 8,height * 17 / 32);
+        textPath.lineTo(5 * width / 8,height * 17 / 32);
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
        super.onDraw(canvas);
-        height = getHeight();
-        width = getWidth();
+
         canvas.drawARGB(1,2,3,4);
 
         Log.e(TAG, "init: next  " + height +  "   " + width  );
-        canvas.drawRect(width / 4,height / 3 ,3 * width / 4,height/2,mPaint);
+        //new RectF(3 * width / 8,height / 3,5 * width / 8,height / 2)
+        canvas.drawRect(width / 4,height / 2 - width / 8 ,3 * width / 4, height / 2 + width / 6,mPaint);
         //canvas.drawText("Loading",3 * width / 8,7 * height / 12,5 * width / 8,7 * height / 12,textPaint);
-        canvas.drawArc(new RectF(3 * width / 8,height / 3,5 * width / 8,height / 2),-180,180,false,mPaint);
+       // canvas.drawArc(new RectF(3 * width / 8,height / 3,5 * width / 8,height / 2),180,190,false,mPaint);
+        canvas.drawPath(path, mPaint);
+        canvas.drawTextOnPath("Loading",textPath,0,-8,textPaint);
+
     }
 }
 
